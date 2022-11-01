3dd47d3d3759ac9ce46706103db0c53ac3e9c20d
Joel Marinho <joeldeveloper2011140@gmail.com>
2022-11-01 11:07:36 +0100

    feat: insert swagger route

--- src/utils/middlewares.ts
+++ src/utils/middlewares.ts
@@ -5,6 +5,12 @@ import compression from 'compression'
 import morgan from 'morgan'
 import { errorMiddleware, erro404 } from './ErrorHandler'
 import { appRoutes } from '../app.routes'
+import swaggerUi from 'swagger-ui-express'
+import { resolve } from 'path'
+import { readFileSync } from 'fs'
+
+const swaggerFile = resolve(__dirname, 'swagger.json')
+const swaggerData = JSON.parse(readFileSync(swaggerFile, 'utf-8'))
 
 export const useMiddlewares = (app: Application): void => {
   app.use(compression())
@@ -17,6 +23,7 @@ export const useMiddlewares = (app: Application): void => {
   }))
   app.use(morgan('dev'))
   // rotas
+  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerData))
   app.use('/api', appRoutes)
   // erros
   app.use(erro404)
